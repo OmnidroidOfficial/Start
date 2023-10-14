c42413451ae8a0a0854c7c74fd0bfe85551cdeb3
OmnidroidOfficial <144283173+OmnidroidOfficial@users.noreply.github.com>
2023-10-14 02:59:02 +0200

    Create start.Shared - PreSort.cs

---
+++ start.Shared - PreSort.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Omnidroid_Start.Shared;
+
+namespace Omnidroid_Start.Shared
+{
+    public class PreSort
+    {
+        public static string? VarA { get; set; }
+        static public bool? ReviewOne { get; set; }
+        public static bool? VarB { get; set; }
+    }
+    
+}
